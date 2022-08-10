1c9da0072f5072b0e232ffbdd6b5519677423c83
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-08-10 13:36:53 +0530

    Create task_76.m

---
+++ task_76.m
@@ -0,0 +1,6 @@
+%a professor took exam of 10 students and now he wants to give them a grade
+%according to the their performance, he will give input of the marks and
+%the grade will be automatically calculated. but there were some students
+%who were abscent in the exam and are going to retake the exam so the
+%professor wants to skip those students and does not want to give them a
+%zero in exam.
