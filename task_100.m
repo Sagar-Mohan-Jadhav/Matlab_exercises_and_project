47b08333638a552c3c729acc559c7b7322de7122
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-08-16 19:05:44 +0530

    Create task_100.m

---
+++ task_100.m
@@ -0,0 +1,23 @@
+% find the smallest summation number with 20 or more than 20 divisors.
+% summation number: summation number is sum of natural numbers less than or
+% equal to that number. ex, n6 = 1 + 2 + 3 + 4 + 5 + 6 = 21 and has 1, 21,
+% 3, 7 total 4 divisors.
+
+count = 2;
+divisors = [];
+while length(divisors) < 21
+    sum = 0;
+    for i = 1: count
+        sum = sum + i;
+    end
+    divisors = [sum];
+    for i = sum/2: -1: 1
+        if rem(sum, i) == 0
+            divisors = [divisors i];
+        end
+    end
+    count = count + 1;
+end
+
+fprintf('The smallest summation number with 20 or more than 20 divisors is %d and divisors are as follows.\n', sum)
+disp(divisors)
