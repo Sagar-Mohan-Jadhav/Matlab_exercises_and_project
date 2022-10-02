0b629777ae4547ec36c7ebfd35d30171774420c1
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-10-02 11:18:53 +0530

    update added function to import data

--- Mini_project_Insurrance_predictor/Insurrance_predictor.m
+++ Mini_project_Insurrance_predictor/Insurrance_predictor.m
@@ -3,7 +3,7 @@ This is a mini project to predict whether a person with given input age and
 salary will buy insurrance or not from a company.
 For this we have a data of 400 people. (Insurrance_data.csv)
 %}
-
+Insurrancedata = importfile("Insurrance_data.csv");
 insurrancedata = table2cell(Insurrancedata);
 insurrance_age = insurrancedata(:, 1);
 insurrance_age = cell2mat(insurrance_age);
@@ -15,9 +15,13 @@ np_age = function_return(:, 1);
 p_age = function_return(:, 2);
 np_salary = function_return(:, 3);
 p_salary = function_return(:, 4);
-plot(np_salary, np_age, "LineStyle","none", "Marker","*", "Color",[1, 0, 0])
+new_person_age = input("Enter the age of the new customer you want to sell insurrance to\n");
+new_person_salary = input("Enter the annual salary of the new customer you want to sell insurrance to\n");
+
+plot(np_salary, np_age, "LineStyle","none", "Marker","*", "Color",[0, 1, 0])
 legend("not insurred")
 hold on
 plot(p_salary, p_age, "LineStyle","none", "Marker","*", "Color",[0, 0, 1])
+plot(new_person_salary, new_person_age, "LineStyle", "none", "Marker", "square", "Color", [0.7 0 0], "LineWidth", 2)
 hold off
-legend("Not insurred", "Insurred")
+legend("Not insurred", "Insurred", "New customer")
