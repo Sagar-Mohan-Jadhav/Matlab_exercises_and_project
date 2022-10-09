7fcf2fcbd3aa0e442fae7c80d6dce5198d7515b8
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-10-10 00:22:39 +0200

    minor update

--- Mini_project_German_Learning_Tool/capitalize_first_letter.m
+++ Mini_project_German_Learning_Tool/capitalize_first_letter.m
@@ -1,3 +1,3 @@
 function capitalized_word = capitalize_first_letter(word)
-capitalized_word = strcat(upper(word(1)), lower(word(2:end)));
+capitalized_word = strcat(upper(word(1)), lower(word(2: end)));
 end
