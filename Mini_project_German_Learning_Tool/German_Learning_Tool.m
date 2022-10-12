88c6adb8c946f21267cf415cdb40669c9697cf59
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-10-12 17:56:36 +0200

    update 3

--- Mini_project_German_Learning_Tool/German_Learning_Tool.m
+++ Mini_project_German_Learning_Tool/German_Learning_Tool.m
@@ -10,56 +10,108 @@ added function-
 incorrectly guessed, till the user corrects it.
 2. now it can ask english word and asks user to type in the german meaning
 of that word. with added function 1 also applicable.
+3. adding newer words to a separate section in excel file.
 %}
-
+old_new = input("Chose one of the following:\n1- Practice new words\n2- Practice old words.\n");
+mode = input("Choose function:\n1- Guess English meaning\n2- Guess German meaning\n");
 while true
-    mode = input("Choose function:\n1- Guess English meaning\n2- Guess German meaning\n");
-    german_vocab = importfile("German_vocab.xlsx");
-    german_vocab = table2cell(german_vocab);
-    german_word = string(german_vocab(:, 1));
-    english_meaning = string(german_vocab(:, 2));
-    incorrect_guess = [];
-    if mode == 1
-        for index = 1: 5
-            random_integer = randi(length(german_word));
-            fprintf('What is the English meaning of word: %s\n', german_word(random_integer))
-            meaning = input("", 's');
-            meaning = capitalize_first_letter(meaning);
-            actual_meaning = english_meaning(random_integer);
-            if string(meaning) == string(capitalize_first_letter(actual_meaning{1}))
-                disp("Correct!")
-            else
-                fprintf("Wrong! The correct meaning is: %s\n", english_meaning(random_integer))
-                incorrect_guess = [incorrect_guess random_integer];
+    if old_new == 2
+        german_vocab = importfile("German_vocab.xlsx");
+        german_vocab = table2cell(german_vocab);
+        german_word = string(german_vocab(:, 1));
+        english_meaning = string(german_vocab(:, 2));
+        incorrect_guess = [];
+        if mode == 1
+            for index = 1: 7
+                random_integer = randi(length(german_word));
+                fprintf('What is the English meaning of word: %s\n', german_word(random_integer))
+                meaning = input("", 's');
+                meaning = capitalize_first_letter(meaning);
+                actual_meaning = english_meaning(random_integer);
+                if string(meaning) == string(capitalize_first_letter(actual_meaning{1}))
+                    disp("Correct!")
+                else
+                    fprintf("Wrong! The correct meaning is: %s\n", english_meaning(random_integer))
+                    incorrect_guess = [incorrect_guess random_integer];
+                end
+            end
+            while ~isempty(incorrect_guess)
+                incorrect_guess = repeat_incorrect(german_word, english_meaning, incorrect_guess);
+            end
+            run = input("Want to run again? y or n\n", 's');
+            if run == 'n'
+                break
+            end
+        else
+            for index = 1: 7
+                random_integer = randi(length(english_meaning));
+                fprintf('What is the German meaning of word: %s\n', english_meaning(random_integer))
+                meaning = input("", 's');
+                meaning = capitalize_first_letter(meaning);
+                actual_meaning = german_word(random_integer);
+                if string(meaning) == string(capitalize_first_letter(actual_meaning{1}))
+                    disp("Correct!")
+                else
+                    fprintf("Wrong! The correct meaning is: %s\n", german_word(random_integer))
+                    incorrect_guess = [incorrect_guess random_integer];
+                end
+            end
+            while ~isempty(incorrect_guess)
+                incorrect_guess = repeat_incorrect(english_meaning, german_word, incorrect_guess);
+            end
+            run = input("Want to run again? y or n\n", 's');
+            if run == 'n'
+                break
             end
         end
-        while ~isempty(incorrect_guess)
-            incorrect_guess = repeat_incorrect(german_word, english_meaning, incorrect_guess);
-        end
-        run = input("Want to run again? y or n\n", 's');
-        if run == 'n'
-            break
-        end
-    else
-        for index = 1: 5
-            random_integer = randi(length(english_meaning));
-            fprintf('What is the German meaning of word: %s\n', english_meaning(random_integer))
-            meaning = input("", 's');
-            meaning = capitalize_first_letter(meaning);
-            actual_meaning = german_word(random_integer);
-            if string(meaning) == string(capitalize_first_letter(actual_meaning{1}))
-                disp("Correct!")
-            else
-                fprintf("Wrong! The correct meaning is: %s\n", german_word(random_integer))
-                incorrect_guess = [incorrect_guess random_integer];
+    elseif old_new == 1
+        german_vocab = importfilenew("German_vocab_new.xlsx");
+        german_vocab = table2cell(german_vocab);
+        german_word = string(german_vocab(:, 1));
+        english_meaning = string(german_vocab(:, 2));
+        incorrect_guess = [];
+        if mode == 1
+            for index = 1: 5
+                random_integer = randi(length(german_word));
+                fprintf('What is the English meaning of word: %s\n', german_word(random_integer))
+                meaning = input("", 's');
+                meaning = capitalize_first_letter(meaning);
+                actual_meaning = english_meaning(random_integer);
+                if string(meaning) == string(capitalize_first_letter(actual_meaning{1}))
+                    disp("Correct!")
+                else
+                    fprintf("Wrong! The correct meaning is: %s\n", english_meaning(random_integer))
+                    incorrect_guess = [incorrect_guess random_integer];
+                end
             end
-        end
-        while ~isempty(incorrect_guess)
-            incorrect_guess = repeat_incorrect(english_meaning, german_word, incorrect_guess);
-        end
-        run = input("Want to run again? y or n\n", 's');
-        if run == 'n'
-            break
+            while ~isempty(incorrect_guess)
+                incorrect_guess = repeat_incorrect(german_word, english_meaning, incorrect_guess);
+            end
+            run = input("Want to run again? y or n\n", 's');
+            if run == 'n'
+                break
+            end
+        else
+            for index = 1: 5
+                random_integer = randi(length(english_meaning));
+                fprintf('What is the German meaning of word: %s\n', english_meaning(random_integer))
+                meaning = input("", 's');
+                meaning = capitalize_first_letter(meaning);
+                actual_meaning = german_word(random_integer);
+                if string(meaning) == string(capitalize_first_letter(actual_meaning{1}))
+                    disp("Correct!")
+                else
+                    fprintf("Wrong! The correct meaning is: %s\n", german_word(random_integer))
+                    incorrect_guess = [incorrect_guess random_integer];
+                end
+            end
+            while ~isempty(incorrect_guess)
+                incorrect_guess = repeat_incorrect(english_meaning, german_word, incorrect_guess);
+            end
+            run = input("Want to run again? y or n\n", 's');
+            if run == 'n'
+                break
+            end
         end
     end
 end
