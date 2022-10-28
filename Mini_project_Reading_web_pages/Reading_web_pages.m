998c41fc46d29fca4fc884ad9c2a573bd228f656
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-10-28 20:04:08 +0200

    Create Reading_web_pages.m

---
+++ Mini_project_Reading_web_pages/Reading_web_pages.m
@@ -0,0 +1,15 @@
+%{
+we first ask user the term they want to search then we use combinations of
+words types by user to get the best search results possible.
+to determine the best search result we will look for words searched by user
+on the web page and the web page with most appearances of searched words
+will open.
+%}
+search_term = string(input("Enter the search term that you want to search on web: ", 's'));
+search_words = strsplit(search_term);
+combinations = perms(1: length(search_words));
+
+
+for index = 1: length(search_words(1, :))
+    search_term(index) = join(search_words())
+end
