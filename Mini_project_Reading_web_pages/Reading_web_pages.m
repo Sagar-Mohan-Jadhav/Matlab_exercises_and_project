da1087d66be28ee83c801cd7c21d39177b1a472c
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-10-29 17:10:30 +0200

    finished

--- Mini_project_Reading_web_pages/Reading_web_pages.m
+++ Mini_project_Reading_web_pages/Reading_web_pages.m
@@ -1,12 +1,16 @@
 %{
-we first ask user the term they want to search then we use combinations of
-words types by user to get the best search results possible.
+we first ask user the keywords that they want to search, then we use combinations of
+words typed by user to get the best search results possible.
+The the program will automatically open brave browser on Chrome in
+Incognito-mode and will search the best result possible.
+The system compatible for proper functioning is:
+OS: Windows 10
 to determine the best search result we will look for words searched by user
 on the web page and the web page with most appearances of searched words
-will open.
+will open automatically.
 %}
 clear all
-search_term = string(input("Enter the search term that you want to search on web: ", 's'));
+search_term = lower(string(input("Enter the search term that you want to search on web: ", 's')));
 search_words = strsplit(search_term);
 combinations = perms(1: length(search_words));
 
@@ -16,7 +20,7 @@ for index = 1: length(search_words)
 end
 for index = 1: length(search_term)
     search_term(index) = strcat("https://search.brave.com/search?q=", search_term(index));
-    search_result(index) = string(extractHTMLText(webread(search_term(index))));
+    search_result(index) = lower(string(extractHTMLText(webread(search_term(index)))));
     pause(1);
 end
 
@@ -28,4 +32,5 @@ for index = 1: length(search_result)
         count_list(index) = appearances;
     end
 end
-
+[max_count, max_index] = max(count_list);
+obtain_results(search_words, combinations, max_index);
---
+++ Mini_project_Reading_web_pages/obtain_results.m
@@ -0,0 +1,114 @@
+function obtain_results(search_words, combinations, max_index)
+words =  search_words(combinations(max_index, :));
+for index = 1: length(words)
+    fprintf("%s ", words(index))
+end
+fprintf('\n')
+pause(1)
+import java.awt.Robot;
+import java.awt.event.*;
+robot = java.awt.Robot();
+Screen_size = get(0, 'ScreenSize');
+Loc_search = [357*(Screen_size(3)/1536) 87*(Screen_size(4)/864)];
+robot.mouseMove(1, 1);
+robot.mouseMove(Loc_search(1), Screen_size(4)- Loc_search(2) + 1);
+robot.mousePress(KeyEvent.BUTTON1_MASK);
+Loc_search = [900*(Screen_size(3)/1536) -110*(Screen_size(4)/864)];
+robot.delay(5000)
+robot.mouseMove(Loc_search(1), Screen_size(4)- Loc_search(2) + 1);
+robot.mouseRelease(KeyEvent.BUTTON1_MASK);
+robot.delay(2000)
+robot.keyPress(KeyEvent.VK_CONTROL);
+robot.keyPress(KeyEvent.VK_C);
+robot.keyRelease(KeyEvent.VK_CONTROL);
+robot.keyRelease(KeyEvent.VK_C);
+
+Loc_search = [192*(Screen_size(3)/1536) 18*(Screen_size(4)/864)];
+robot.mouseMove(1, 1);
+robot.mouseMove(Loc_search(1), Screen_size(4)- Loc_search(2) + 1);
+robot.mousePress(KeyEvent.BUTTON1_MASK);
+robot.mouseRelease(KeyEvent.BUTTON1_MASK);
+robot.delay(2000);
+robot.keyPress(KeyEvent.VK_C);
+robot.keyRelease(KeyEvent.VK_C);
+robot.keyPress(KeyEvent.VK_H);
+robot.keyRelease(KeyEvent.VK_H);
+robot.keyPress(KeyEvent.VK_R);
+robot.keyRelease(KeyEvent.VK_R);
+robot.keyPress(KeyEvent.VK_O);
+robot.keyRelease(KeyEvent.VK_O);
+robot.keyPress(KeyEvent.VK_M);
+robot.keyRelease(KeyEvent.VK_M);
+robot.keyPress(KeyEvent.VK_E);
+robot.keyRelease(KeyEvent.VK_E);
+robot.delay(2000);
+robot.keyPress(KeyEvent.VK_ENTER);
+robot.keyRelease(KeyEvent.VK_ENTER);
+robot.delay(3000);
+robot.keyPress(KeyEvent.VK_CONTROL);
+robot.keyPress(KeyEvent.VK_SHIFT);
+robot.keyPress(KeyEvent.VK_N);
+robot.keyRelease(KeyEvent.VK_CONTROL);
+robot.keyRelease(KeyEvent.VK_SHIFT);
+robot.keyRelease(KeyEvent.VK_N);
+robot.delay(2000);
+robot.keyPress(KeyEvent.VK_B);
+robot.keyRelease(KeyEvent.VK_B);
+robot.keyPress(KeyEvent.VK_R);
+robot.keyRelease(KeyEvent.VK_R);
+robot.keyPress(KeyEvent.VK_A);
+robot.keyRelease(KeyEvent.VK_A);
+robot.keyPress(KeyEvent.VK_V);
+robot.keyRelease(KeyEvent.VK_V);
+robot.keyPress(KeyEvent.VK_E);
+robot.keyRelease(KeyEvent.VK_E);
+robot.keyPress(KeyEvent.VK_SPACE);
+robot.keyRelease(KeyEvent.VK_SPACE);
+robot.keyPress(KeyEvent.VK_S);
+robot.keyRelease(KeyEvent.VK_S);
+robot.keyPress(KeyEvent.VK_E);
+robot.keyRelease(KeyEvent.VK_E);
+robot.keyPress(KeyEvent.VK_A);
+robot.keyRelease(KeyEvent.VK_A);
+robot.keyPress(KeyEvent.VK_R);
+robot.keyRelease(KeyEvent.VK_R);
+robot.keyPress(KeyEvent.VK_C);
+robot.keyRelease(KeyEvent.VK_C);
+robot.keyPress(KeyEvent.VK_H);
+robot.keyRelease(KeyEvent.VK_H);
+robot.keyPress(KeyEvent.VK_ENTER);
+robot.keyRelease(KeyEvent.VK_ENTER);
+robot.delay(700);
+robot.keyPress(KeyEvent.VK_TAB);
+robot.keyRelease(KeyEvent.VK_TAB);
+robot.delay(700);
+robot.keyPress(KeyEvent.VK_TAB);
+robot.keyRelease(KeyEvent.VK_TAB);
+robot.delay(700);
+robot.keyPress(KeyEvent.VK_TAB);
+robot.keyRelease(KeyEvent.VK_TAB);
+robot.delay(700);
+robot.keyPress(KeyEvent.VK_TAB);
+robot.keyRelease(KeyEvent.VK_TAB);
+robot.delay(700);
+robot.keyPress(KeyEvent.VK_TAB);
+robot.keyRelease(KeyEvent.VK_TAB);
+robot.delay(700);
+robot.keyPress(KeyEvent.VK_ENTER);
+robot.keyRelease(KeyEvent.VK_ENTER);
+robot.delay(700);
+for index = 1: 16
+    robot.keyPress(KeyEvent.VK_TAB);
+    robot.keyRelease(KeyEvent.VK_TAB);
+end
+robot.keyPress(KeyEvent.VK_ENTER);
+robot.keyRelease(KeyEvent.VK_ENTER);
+robot.delay(2000);
+robot.keyPress(KeyEvent.VK_CONTROL);
+robot.keyPress(KeyEvent.VK_V);
+robot.keyRelease(KeyEvent.VK_CONTROL);
+robot.keyRelease(KeyEvent.VK_V);
+robot.delay(700);
+robot.keyPress(KeyEvent.VK_ENTER);
+robot.keyRelease(KeyEvent.VK_ENTER);
+end
