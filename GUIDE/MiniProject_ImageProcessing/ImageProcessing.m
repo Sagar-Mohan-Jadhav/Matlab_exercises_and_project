a45f376234b4d02ee1474bcf028fb421476b3150
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-11-10 23:52:10 +0100

    brightness function added

--- GUIDE/MiniProject_ImageProcessing/ImageProcessing.m
+++ GUIDE/MiniProject_ImageProcessing/ImageProcessing.m
@@ -22,7 +22,7 @@ function varargout = ImageProcessing(varargin)
 
 % Edit the above text to modify the response to help ImageProcessing
 
-% Last Modified by GUIDE v2.5 10-Nov-2022 23:05:00
+% Last Modified by GUIDE v2.5 10-Nov-2022 23:21:17
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -79,6 +79,8 @@ function load_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 global image
+global change
+change = 0;
 [filename, filepath] = uigetfile({'*.jpg'}, 'File Selector');
 filepath = strcat(filepath, filename);
 image = imread(filepath);
@@ -93,8 +95,9 @@ function gray_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 global image
+global change
 axes(handles.axes1);
-grayimg = rgb2gray(image);
+grayimg = rgb2gray(image + change);
 imshow(grayimg);
 
 
@@ -104,7 +107,37 @@ function negative_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 global image
+global change
 levels = 2 ^ 8;
-negimg = (levels - 1) - image;
+negimg = (levels - 1) - (image + change);
 axes(handles.axes1);
 imshow(negimg);
+
+
+% --- Executes on slider movement.
+function brightness_Callback(hObject, eventdata, handles)
+% hObject    handle to brightness (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+global image
+global change
+slidervalue = get(handles.brightness, 'value');
+brightimage = slidervalue * 2 * image;
+axes(handles.axes1)
+imshow(brightimage)
+change = brightimage - image;
+
+
+% --- Executes during object creation, after setting all properties.
+function brightness_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to brightness (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
