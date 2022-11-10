42d2b819563eddd02517b6bdb0b5dfaa0c87487c
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-11-10 23:12:35 +0100

    mini project image processing

---
+++ GUIDE/MiniProject_ImageProcessing/ImageProcessing.m
@@ -0,0 +1,110 @@
+function varargout = ImageProcessing(varargin)
+% IMAGEPROCESSING MATLAB code for ImageProcessing.fig
+%      IMAGEPROCESSING, by itself, creates a new IMAGEPROCESSING or raises the existing
+%      singleton*.
+%
+%      H = IMAGEPROCESSING returns the handle to a new IMAGEPROCESSING or the handle to
+%      the existing singleton*.
+%
+%      IMAGEPROCESSING('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in IMAGEPROCESSING.M with the given input arguments.
+%
+%      IMAGEPROCESSING('Property','Value',...) creates a new IMAGEPROCESSING or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before ImageProcessing_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to ImageProcessing_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help ImageProcessing
+
+% Last Modified by GUIDE v2.5 10-Nov-2022 23:05:00
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ImageProcessing_OpeningFcn, ...
+                   'gui_OutputFcn',  @ImageProcessing_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before ImageProcessing is made visible.
+function ImageProcessing_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ImageProcessing (see VARARGIN)
+
+% Choose default command line output for ImageProcessing
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes ImageProcessing wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ImageProcessing_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in load.
+function load_Callback(hObject, eventdata, handles)
+% hObject    handle to load (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global image
+[filename, filepath] = uigetfile({'*.jpg'}, 'File Selector');
+filepath = strcat(filepath, filename);
+image = imread(filepath);
+axes(handles.axes1);
+imshow(image);
+
+
+
+% --- Executes on button press in gray.
+function gray_Callback(hObject, eventdata, handles)
+% hObject    handle to gray (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global image
+axes(handles.axes1);
+grayimg = rgb2gray(image);
+imshow(grayimg);
+
+
+% --- Executes on button press in negative.
+function negative_Callback(hObject, eventdata, handles)
+% hObject    handle to negative (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global image
+levels = 2 ^ 8;
+negimg = (levels - 1) - image;
+axes(handles.axes1);
+imshow(negimg);
