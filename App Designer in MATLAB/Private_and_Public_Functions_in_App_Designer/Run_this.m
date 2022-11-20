87d08dd3e441c534b87a3e4d137c49054593a352
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-11-20 11:21:28 +0100

    Private and Public Functions

---
+++ App Designer in MATLAB/Private_and_Public_Functions_in_App_Designer/Run_this.m
@@ -0,0 +1,7 @@
+% To run the public function, call it in this script as pub_wave(Simple_wave_function,model_type).
+% model_type for different waves is as follows.
+% 0 ------ sine wave
+% 1 ------ cosine wave
+% 2 (or any other number) -------- tangent wave
+% ex. to display cosine wave, type below, pub_wave(Simple_wave_function, 1)
+pub_wave(Simple_wave_function, 5);
